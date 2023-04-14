dc1ee7f75bfcc250928973be96d69a8bfce2bcca
mikolaj <mikolaj.debowski@theta.co.nz>
2023-04-14 12:14:51 +1200

    first step

---
+++ example/lib/main.dart
@@ -0,0 +1,138 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_recaptcha_enterprise/flutter_recaptcha_enterprise.dart';
+import 'package:flutter_recaptcha_enterprise/recaptcha_action.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final flutterRecaptchaEnterprise = FlutterRecaptchaEnterprise();
+
+  String result = "action";
+  String score = "unknown";
+
+  Future<void> initPlatformState() async {
+    String envRaw = await rootBundle.loadString('assets/env.json');
+    var env = json.decode(envRaw) as Map<String, dynamic>;
+
+    String siteKeyAndroid = env["siteKeyAndroid"];
+    String siteKeyIOS = env["siteKeyIOS"];
+
+    if (Platform.isAndroid) {
+      await flutterRecaptchaEnterprise.setupAndroid(siteKeyAndroid);
+    } else {
+      await flutterRecaptchaEnterprise.setupIOS(siteKeyIOS);
+    }
+  }
+
+  Future<double?> getRecaptchaScore(String token) async {
+    String envRaw = await rootBundle.loadString('assets/env.json');
+    var env = json.decode(envRaw) as Map<String, dynamic>;
+
+    String siteKeyIOS = env["siteKeyIOS"];
+    String projectId = env["PROJECT_ID"];
+    String apiKey = env["API_KEY"];
+    String siteKeyAndroid = env["siteKeyAndroid"];
+
+    var siteKey = "";
+    if (Platform.isAndroid) {
+      siteKey = siteKeyAndroid;
+    } else {
+      siteKey = siteKeyIOS;
+    }
+
+    final recaptchaApiUrl = 'https://recaptchaenterprise.googleapis.com/v1/projects/$projectId/assessments?key=$apiKey';
+    final requestBody = json.encode({
+      "event": {
+        "token": token,
+        "siteKey": siteKey,
+        "expectedAction": "login",
+      },
+    });
+    final response = await http.post(
+      Uri.parse(recaptchaApiUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: requestBody,
+    );
+
+    final result = json.decode(response.body);
+    result.toString().log();
+    double score = result['riskAnalysis']['score'] as double;
+    return score;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: () async {
+                    await initPlatformState();
+
+                    setState(() {
+                      result = "ok";
+                    });
+                  },
+                  child: const Text("setup"),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    String customAction = "test";
+
+                    String result = await flutterRecaptchaEnterprise.execute(RecaptchaAction.custom(customAction));
+
+                    setState(() {
+                      // log(result);
+                      this.result = result;
+                    });
+                  },
+                  child: Text(result),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    final token = result;
+                    final tempScore = await getRecaptchaScore(token);
+                    setState(() {
+                      score = tempScore.toString();
+                    });
+                    print('reCAPTCHA score: $score');
+                  },
+                  child: Text("get the score: $score"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/flutter_recaptcha_enterprise.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:flutter_recaptcha_enterprise/recaptcha_action.dart';
+
+import 'flutter_recaptcha_enterprise_platform_interface.dart';
+
+class FlutterRecaptchaEnterprise {
+  Future<String> setupAndroid(String siteKeyAndroid) async {
+    return await FlutterRecaptchaEnterprisePlatform.instance
+        .setupAndroid(siteKeyAndroid) as String;
+  }
+
+  Future<String> setupIOS(String siteKeyIOS) async {
+    print("setupIOS");
+    return await FlutterRecaptchaEnterprisePlatform.instance
+        .setupIOS(siteKeyIOS) as String;
+  }
+
+  Future<String> execute(RecaptchaAction action) async {
+    print("execute");
+    return await FlutterRecaptchaEnterprisePlatform.instance.execute(action)
+        as String;
+  }
+}
